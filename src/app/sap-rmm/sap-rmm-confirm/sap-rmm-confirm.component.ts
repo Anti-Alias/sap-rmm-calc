92f6e9503a342ea2173bf3a6576f15da4bc00604
Anti_Alias <drewlikesramen@gmail.com>
2020-10-05 12:07:55 -0400

    Moved tabular data to new component

--- src/app/sap-rmm/sap-rmm-confirm/sap-rmm-confirm.component.ts
+++ src/app/sap-rmm/sap-rmm-confirm/sap-rmm-confirm.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 import { SAPRMMService } from '../../services/sap-rmm.service';
 import { MatDialog } from '@angular/material/dialog'
 import { SubmitConfirmDialogComponent } from 'src/app/submit-confirm-dialog/submit-confirm-dialog.component';
@@ -7,25 +7,12 @@ import { filter, mergeMap } from 'rxjs/operators'
 import { Router } from '@angular/router';
 import { AcknowledgeDialogComponent } from 'src/app/acknowledge-dialog/acknowledge-dialog.component';
 
-export interface SAPRMMSubData {
-  saprmmid: string;
-  upb: number;
-  rmm: string;
-  loanStatus: string,
-  upbCurrentAmount: number,
-  maturityDate: Date,
-  poolTerm: number
-}
-
 @Component({
   selector: 'app-sapp-rmm-confirm',
   styleUrls: ['sap-rmm-confirm.component.css'],
   templateUrl: 'sap-rmm-confirm.component.html',
 })
-export class SapRmmConfirmComponent implements OnInit {
-
-  displayedColumns: string[] = ['saprmmid', 'upb', 'rmm', 'loanStatus', 'upbCurrentAmount', 'maturityDate', 'poolTerm'];
-  dataSource: SAPRMMSubData[]
+export class SapRmmConfirmComponent {
 
   constructor(
     public dialog: MatDialog,
@@ -33,21 +20,6 @@ export class SapRmmConfirmComponent implements OnInit {
     private dataStorageService: DataStorageService,
     private router: Router
   ) {}
-
-  ngOnInit(): void {
-    const data = this.saprmmService.formState
-    this.dataSource = [
-      {
-        saprmmid: "STUB",
-        upb: data.upbAdjustmentAmountCurrent,
-        rmm: "STUB",
-        loanStatus: data.loanStatus,
-        upbCurrentAmount: data.upbCurrentAmount,
-        maturityDate: data.maturityDate,
-        poolTerm: data.poolTerm
-      }
-    ]
-  }
 
   onConfirm(event: Event): void {
     this.dialog
--- src/app/sap-rmm/sap-rmm-display/sap-rmm-display.component.ts
+++ src/app/sap-rmm/sap-rmm-display/sap-rmm-display.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { SAPRMMService } from 'src/app/services/sap-rmm.service';
 
+
+/**
+ * Represents a reusable component that displays SapRmm data in tabular form with pagination.
+ */
 @Component({
   selector: 'app-sap-rmm-display',
   templateUrl: './sap-rmm-display.component.html',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SapRmmDisplayComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['saprmmid', 'upb', 'rmm', 'loanStatus', 'upbCurrentAmount', 'maturityDate', 'poolTerm'];
+  dataSource: SAPRMMSubData[]
+
+  constructor(private saprmmService: SAPRMMService) {}
 
   ngOnInit(): void {
+    const data = this.saprmmService.formState
+    this.dataSource = [
+      {
+        saprmmid: "STUB",
+        upb: data.upbAdjustmentAmountCurrent,
+        rmm: "STUB",
+        loanStatus: data.loanStatus,
+        upbCurrentAmount: data.upbCurrentAmount,
+        maturityDate: data.maturityDate,
+        poolTerm: data.poolTerm
+      }
+    ]
   }
+}
 
+/**
+ * A subset of the SAPRMM model data that is to be displayed on the screen.
+ */
+interface SAPRMMSubData {
+  saprmmid: string;
+  upb: number;
+  rmm: string;
+  loanStatus: string,
+  upbCurrentAmount: number,
+  maturityDate: Date,
+  poolTerm: number
 }
